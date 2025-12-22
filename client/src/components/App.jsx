532ba28026d2dfb2a4ff7d942eddd74cabd17283
Karolina F-D <karolinafonk@gmail.com>
2025-12-22 19:10:13 +0100

    first setup with simple stuff

---
+++ client/src/components/App.jsx
@@ -0,0 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
+import Post from "./Post";
+
+export function App() {
+  const { data = { baz: "astral" } } = useQuery({
+    queryKey: ["hello"],
+    queryFn: async () => {
+      const res = await fetch("http://localhost:5002/api/hello");
+      return res.json();
+    },
+  });
+
+  const posts = [
+    { id: 1, title: "Post 1 Headline", text: "Lorem ipsum lalalalala" },
+    { id: 2, title: "Post 2 Headline", text: "Lorem ipsum lalalalala" },
+    { id: 3, title: "Post 3 Headline", text: "Lorem ipsum lalalalala" },
+    { id: 4, title: "Post 4 Headline", text: "Lorem ipsum lalalalala" },
+  ];
+
+  return (
+    <div>
+      <div className="container">
+        {posts.map((post) => (
+          <Post key={post.id} title={post.title} text={post.text} />
+        ))}
+      </div>
+      {/* <div>{JSON.stringify(data)}</div> */}
+    </div>
+  );
+}
---
+++ client/src/components/Post.jsx
@@ -0,0 +1,15 @@
+function Post(props) {
+  return (
+    <div className="post">
+      <div className="picture">
+        <img src="../assets/bear.jpg" alt="sample picture" />
+      </div>
+      <div className="content">
+        <div className="title"> {props.title}</div>
+        <p className="text">{props.text}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Post;
